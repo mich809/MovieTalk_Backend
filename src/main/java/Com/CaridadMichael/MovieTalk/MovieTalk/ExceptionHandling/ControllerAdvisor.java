9a1ffb3f840d23cf05f80113468fbefbcbdf4161
Michael Caridad <caridadmichael@gmail.com>
2021-11-19 17:21:41 -0500

    test file

---
+++ src/main/java/Com/CaridadMichael/MovieTalk/MovieTalk/ExceptionHandling/ControllerAdvisor.java
@@ -0,0 +1,9 @@
+package Com.CaridadMichael.MovieTalk.MovieTalk.ExceptionHandling;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ControllerAdvisor extends ResponseEntityExceptionHandler {
+
+}
