0eb8117e77c47e529611a31b319fd1cae51a8bc4
Michael Caridad <caridadmichael@gmail.com>
2021-11-24 01:29:57 -0500

    changed relationship between user and movie

--- src/main/java/Com/CaridadMichael/MovieTalk/MovieTalk/Entities/Movie.java
+++ src/main/java/Com/CaridadMichael/MovieTalk/MovieTalk/Entities/Movie.java
@@ -3,13 +3,13 @@ package Com.CaridadMichael.MovieTalk.MovieTalk.Entities;
 
 
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.Entity;
-
+import javax.persistence.FetchType;
 import javax.persistence.Id;
-
-
-
-
+import javax.persistence.ManyToMany;
 
 import lombok.Data;
 
@@ -18,7 +18,10 @@ import lombok.Data;
 @Data
 public class Movie {
 		@Id
-	    private String title; 		
+	    private String title; 	
+		
+		@ManyToMany(mappedBy = "movies", fetch = FetchType.EAGER)
+	    private Set<User> users = new HashSet<User>();
 		
 	
 		
