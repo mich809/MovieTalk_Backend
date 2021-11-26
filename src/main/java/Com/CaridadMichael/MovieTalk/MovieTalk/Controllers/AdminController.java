69893d55c9fecb3e015e4e19421445c81c9dc06a
Michael Caridad <caridadmichael@gmail.com>
2021-11-26 02:54:39 -0500

    added deleteUser endpoint

--- src/main/java/Com/CaridadMichael/MovieTalk/MovieTalk/Controllers/AdminController.java
+++ src/main/java/Com/CaridadMichael/MovieTalk/MovieTalk/Controllers/AdminController.java
@@ -1,6 +1,7 @@
 package Com.CaridadMichael.MovieTalk.MovieTalk.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,28 +10,32 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import Com.CaridadMichael.MovieTalk.MovieTalk.Entities.User;
-import Com.CaridadMichael.MovieTalk.MovieTalk.Service.UserService;
+import Com.CaridadMichael.MovieTalk.MovieTalk.Service.AdminService;
+
 
 @RestController
 @RequestMapping("admin")
+@PreAuthorize("hasRole('Admin')")
 public class AdminController {
 	
 	@Autowired
-	private UserService userService;
+	private AdminService adminService;
 	
 	
-	@GetMapping("/findUser/{id}")
-    @PreAuthorize("hasRole('Admin')")
+	@GetMapping("/findUser/{id}") 
     public User getUser(@RequestParam String id) {
-    	return userService.getUser(id);
+    	return adminService.getUser(id);
     
     }
 	
-	@DeleteMapping("/findUser/{id}")
-	@PreAuthorize("hasRole('Admin')")
-    public User deletetUser(@RequestParam String id) {
-    	return null;
+	@DeleteMapping("/deleteUser/{id}")    
+    public ResponseEntity<String> deleteUser(@RequestParam String id) {
+    	return adminService.deleteUser(id);
     
     }
+	
+	
+	
+
 
 }
