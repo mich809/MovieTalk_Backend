5a46c7e918659d4c70bb87e3763211efecef1ba5
Michael Caridad <caridadmichael@gmail.com>
2021-12-15 16:34:24 -0500

    added more endpoints

--- src/main/java/Com/CaridadMichael/MovieTalk/MovieTalk/Controllers/MovieController.java
+++ src/main/java/Com/CaridadMichael/MovieTalk/MovieTalk/Controllers/MovieController.java
@@ -3,13 +3,13 @@ package Com.CaridadMichael.MovieTalk.MovieTalk.Controllers;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,49 +20,58 @@ import Com.CaridadMichael.MovieTalk.MovieTalk.Entities.Movie;
 import Com.CaridadMichael.MovieTalk.MovieTalk.Service.MovieService;
 
 
+
 @RestController
 @RequestMapping("movie")
+@PreAuthorize("hasRole('User')")
 public class MovieController {
 	
  	@Autowired
     private MovieService movieService;
-	
- 	@PutMapping("/movie/rateMovie/{rating}")
-    @PreAuthorize("hasRole('User')")
-    public ResponseEntity<String> ratedMovie(@RequestParam int rating, @RequestParam String id) {    	
-    	
- 		return movieService.rateMovie(rating, id);    	
-    }	
-	
-	 @GetMapping("/getMovies")
-	public ResponseEntity<Set<Movie>> getMovies(Authentication authentication) {    	
+ 	
+	 	@PostMapping("/postRating")
+		public void postRating(@RequestParam String id ,@RequestParam int rating) { 	
+	 		 movieService.rateMovie(id, rating);
+		}
 		
-		 return movieService.getFavoriteMovies(authentication.getName());
-	}
+ 	
+	
+		 @GetMapping("/getMovies")
+		 public ResponseEntity<Set<Movie>> getMovies(Authentication authentication) {    	
+				return movieService.getFavoriteMovies(authentication.getName());
+		}
 	 
-	 @PostMapping("/likeMovie")
-    @PreAuthorize("hasRole('User')")
-    public ResponseEntity<Movie> likedMovie(@RequestBody Movie movie,Authentication authentication) {    	
-    	
-		 return movieService.likedMovie(movie,authentication.getName());  
-    }
+	   @PostMapping("/likeMovie") 
+		public ResponseEntity<String> likedMovie(@RequestBody Movie movie,Authentication authentication) {    	
+		 
+			 return movieService.likedMovie(movie,authentication.getName());  
+		    }
 	 
-	@DeleteMapping("/removeMovie")
-    @PreAuthorize("hasRole('User')")
-    public ResponseEntity<String> removeMovie(@RequestBody Movie movie,Authentication authentication) {   
+		@DeleteMapping("/removeMovie")   
+	    public ResponseEntity<String> removeMovie(@RequestParam String id,Authentication authentication) {   
+			
+			return movieService.removeMovie(id,authentication.getName());      	
+	    }
 		
-		return movieService.removeMovie(movie,authentication.getName());      	
-    }
+		@PostMapping("/postComment")	
+		public ResponseEntity<String> comment(@RequestBody Comment comment,@RequestParam String id) {    	
+		    	return  movieService.commentMovie(comment , id);  
+		}
 	
-	@PostMapping("/comment")
-	@PreAuthorize("hasRole('User')")
-	public ResponseEntity<String> comment(@RequestBody Comment comment,@RequestParam String id) {    	
-	    	return movieService.commentMovie(comment , id);  
-	}
-	
-	 @GetMapping("/getComments")
-		public ResponseEntity<Set<Comment>> getComments(@RequestParam String id) {    	
+	    @GetMapping("/getComments")
+		public Set<Comment> getComments(@RequestParam String id) {    	
 			
 			 return movieService.getAllComments(id);
+		}	 
+	 	
+	 	
+	 	@GetMapping("/getRating")
+		public int getRating(@RequestParam String id) { 	
+			return movieService.getMovieRating(id);
+		}
+	 	
+	 	@GetMapping("/likedStatus")
+	 	public boolean getLikedStatus(@RequestParam String id , Authentication authentication) { 	
+			return movieService.getMovieLikedStatus(id, authentication.getName());
 		}
 }
